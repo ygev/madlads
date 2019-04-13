14ea3c36d2934b736b619637169659108ee4db4a
yana <ygev@outlook.com>
2019-04-13 16:24:46 -0400

    Flip camera and adjust Cat again.

--- sketch.js
+++ sketch.js
@@ -20,6 +20,7 @@ function setup() {
   noCanvas();
   // Create a camera input
   video = createCapture(VIDEO);
+  filter("INVERT");
   // Initialize the Image Classifier method with MobileNet and the video as the second argument
   classifier = ml5.imageClassifier("MobileNet", video, modelReady);
 }
