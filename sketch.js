72b99e10154967faa0fa3cb9257fb8e1f6262a94
yana <ygev@outlook.com>
2019-04-13 03:14:27 -0400

    Revert.

--- sketch.js
+++ sketch.js
@@ -19,15 +19,6 @@ const myVoice = new p5.Speech();
 function setup() {
   noCanvas();
   // Create a camera input
-  createCapture({
-    audio: false,
-    video: {
-      facingMode: {
-        exact: "environment"
-      }
-    }
-  });
-
   video = createCapture(VIDEO);
   // Initialize the Image Classifier method with MobileNet and the video as the second argument
   classifier = ml5.imageClassifier("MobileNet", video, modelReady);
