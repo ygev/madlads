8f87c9f222f5a8c235d2e8f8e951bfc276e5cf50
yana <ygev@outlook.com>
2019-04-13 20:40:27 -0400

    Set up constraints.

--- sketch.js
+++ sketch.js
@@ -19,14 +19,14 @@ const myVoice = new p5.Speech();
 function setup() {
   noCanvas();
   // Create a camera input
-  video = createCapture(VIDEO);
-  filter("INVERT");
-  createCapture({
+  var constraints = {
     audio: false,
     video: {
-      facingMode: "environment"
+      facingMode: "user"
     }
-  });
+  };
+  video = createCapture(VIDEO);
+  filter("INVERT");
   // Initialize the Image Classifier method with MobileNet and the video as the second argument
   classifier = ml5.imageClassifier("MobileNet", video, modelReady);
 }
